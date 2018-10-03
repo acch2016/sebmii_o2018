2613f247611888b8c4691f57f7d7d9c2f900f990
acch2016 <ie696846@iteso.mx>
2018-10-03 17:21:29 -0500

    stage

--- hw4/source/rtos_gpio.h
+++ hw4/source/rtos_gpio.h
@@ -30,4 +30,5 @@ rtos_gpio_flag_t rtos_gpio_init(rtos_gpio_config_t config);
 //TODO Esta función recibira la estructura tipo rtos_gpio_config_t de donde obtendria el pin y el puerto
 rtos_gpio_flag_t rtos_gpio_wait_pin(rtos_gpio_config_t config);
 //TODO Declarar aqui la otra función que controlará al LED
+rtos_gpio_flag_t rtos_gpio_toogle_pin(rtos_gpio_config_t config);
 #endif /* RTOS_GPIO_H_ */
